53eb67406f277b5fb57776a2c3ee9b13e735d703
f62esreb <118302392+f62esreb@users.noreply.github.com>
2022-12-02 12:31:58 +0100

    Update curso.cc

--- src/curso/curso.cc
+++ src/curso/curso.cc
@@ -2,6 +2,22 @@
 #include <list>
 
 
+Curso::Curso(std::string id, std::string nombre, std::string descripcion, time_t fecha_inicio, time_t fecha_fin, std::string correo_admin_curso,
+             std::string ponentes, std::string requisitos, int participantes, int max_participantes){
+    
+    id_=id;
+    nombre_=nombre;
+    descripcion_=descripcion;
+    fecha_inicio_=fecha_inicio;
+    fecha_fin_=fecha_fin;
+    correo_admin_curso_=correo_admin_curso;
+    ponentes_=ponentes;
+    requisitos_=requisitos;
+    participantes_=participantes;
+    max_participantes_=max_participantes;
+          
+}
+
 bool Curso::addInscripcion(std::string id_usuario) {
     if ( participantes_ < max_participantes_ ) {
         id_participantes_.push_back(id_usuario);
