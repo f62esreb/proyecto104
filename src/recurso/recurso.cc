7c020d3e29034d6f46bac7221cc38f6fd478d797
f62esreb <118302392+f62esreb@users.noreply.github.com>
2022-12-13 03:11:06 +0100

    Add files via upload

--- src/recurso/recurso.cc
+++ src/recurso/recurso.cc
@@ -1,5 +1,10 @@
 #include"recurso.h"
 
+Recurso::Recurso(std::string id_recurso, std::string nombre_recurso){
+	id_recurso_=id_recurso;
+	nombre_recurso_=nombre_recurso;
+}
+
 bool Recurso::crearRecurso(std::string id_recurso){
 	for (auto it = lista_recursos_.begin(); it != lista_recursos_.end(); it++) {
 	        if ( *it == id_recurso ) {
--- src/recurso/recurso.h
+++ src/recurso/recurso.h
@@ -7,12 +7,18 @@
 class Recurso{
 
 	private:
-		std::string id_recurso_;
+		std::string id_recurso_,nombre_recurso_;
 		std::list<std::string> lista_recursos_;
 	public:
 
+		Recurso(std::string id_recurso, std::string nombre_recurso);
+
+
 		inline std::string get_id(){return id_recurso_;};
 		inline void set_id(std::string id_recurso){id_recurso_=id_recurso;};
+
+		inline std::string get_nombre(){return nombre_recurso_;};
+		inline void set_nombre(std::string nombre_recurso){nombre_recurso_=nombre_recurso;};
 
 		bool crearRecurso(std::string id_recurso);
 };
